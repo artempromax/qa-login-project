37ba424c8a6da02db29f5930748e53c33b6158c2
Artem <atakishievww@gmail.com>
2026-05-13 08:50:19 +0300

    Add movie list and API endpoint for movies
    Added a simplified movie list and API endpoint.

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -12,13 +11,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Подключение к БД
 const db = new sqlite3.Database('./database.db', (err) => {
   if (err) console.error(err.message);
   else console.log('Connected to SQLite database.');
 });
 
-// Создание таблицы пользователей
 db.run(`CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
@@ -29,9 +26,61 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
   password TEXT
 )`);
 
+// 🎬 ТОП фильмы (упрощённый список)
+const movies = [
+  {
+    title: "Побег из Шоушенка",
+    year: 1994,
+    rating: 9.3,
+    poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    link: "https://www.kinopoisk.vip/film/326/"
+  },
+  {
+    title: "Крёстный отец",
+    year: 1972,
+    rating: 9.2,
+    poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    link: "https://www.kinopoisk.vip/film/325/"
+  },
+  {
+    title: "Тёмный рыцарь",
+    year: 2008,
+    rating: 9.0,
+    poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    link: "https://www.kinopoisk.vip/film/111543/"
+  },
+  {
+    title: "Бойцовский клуб",
+    year: 1999,
+    rating: 8.8,
+    poster: "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
+    link: "https://www.kinopoisk.vip/film/361/"
+  },
+  {
+    title: "Начало",
+    year: 2010,
+    rating: 8.8,
+    poster: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    link: "https://www.kinopoisk.vip/film/447301/"
+  },
+  {
+    title: "Интерстеллар",
+    year: 2014,
+    rating: 8.7,
+    poster: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    link: "https://www.kinopoisk.vip/film/258687/"
+  }
+];
+
+// API фильмов
+app.get('/api/movies', (req, res) => {
+  res.json(movies);
+});
+
 // Регистрация
 app.post('/register', (req, res) => {
   const { first_name, last_name, email, age, username, password } = req.body;
+
   db.run(
     `INSERT INTO users (first_name, last_name, email, age, username, password)
      VALUES (?, ?, ?, ?, ?, ?)`,
@@ -50,9 +99,10 @@ app.post('/register', (req, res) => {
   );
 });
 
-// Вход (логин) с возвратом данных пользователя
+// Логин
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+
   db.get(
     `SELECT first_name, last_name, age, username FROM users WHERE username = ? AND password = ?`,
     [username, password],
