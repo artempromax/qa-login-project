dff74e285e50810f5374aa8c490a5a3d1176ed1a
Artem <atakishievww@gmail.com>
2026-05-20 18:24:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -32,14 +32,14 @@ const movies = [
     title: "Побег из Шоушенка",
     year: 1994,
     rating: 9.3,
-    poster: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    poster: "https://www.kinopoisk.ru/picture/2808223/",
     link: "https://www.kinopoisk.vip/film/326/"
   },
   {
     title: "Крёстный отец",
     year: 1972,
     rating: 9.2,
-    poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    poster: "https://www.kinopoisk.ru/picture/3776147/",
     link: "https://www.kinopoisk.vip/film/325/"
   },
   {
@@ -69,7 +69,133 @@ const movies = [
     rating: 8.7,
     poster: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
     link: "https://www.kinopoisk.vip/film/258687/"
-  }
+  },
+  {
+    title: "Форрест Гамп",
+    year: 1994,
+    rating: 8.8,
+    poster: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
+    link: "https://www.kinopoisk.vip/film/448/"
+  },
+  {
+    title: "Матрица",
+    year: 1999,
+    rating: 8.7,
+    poster: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg",
+    link: "https://www.kinopoisk.vip/film/301/"
+  },
+  {
+    title: "Гладиатор",
+    year: 2000,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/ty8TGRuvJLPUmAR1H1nRIsgwvim.jpg",
+    link: "https://www.kinopoisk.vip/film/474/"
+  },
+  {
+    title: "Зелёная миля",
+    year: 1999,
+    rating: 8.9,
+    poster: "https://image.tmdb.org/t/p/w500/velWPhVMQeQKcxggNEU8YmIo52R.jpg",
+    link: "https://www.kinopoisk.vip/film/435/"
+  },
+  {
+    title: "1+1",
+    year: 2011,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/323BP0itpxTsO0skTwdnVmf7YC9.jpg",
+    link: "https://www.kinopoisk.vip/film/535341/"
+  },
+  {
+    title: "Остров проклятых",
+    year: 2010,
+    rating: 8.2,
+    poster: "https://image.tmdb.org/t/p/w500/4GDy0PHYX3VRXUtwK5ysFbg3kEx.jpg",
+    link: "https://www.kinopoisk.vip/film/397667/"
+  },
+  {
+    title: "Джанго освобождённый",
+    year: 2012,
+    rating: 8.4,
+    poster: "https://image.tmdb.org/t/p/w500/7oWY8VDWW7thTzWh3OKYRkWUlD5.jpg",
+    link: "https://www.kinopoisk.vip/film/586397/"
+  },
+  {
+    title: "Паразиты",
+    year: 2019,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+    link: "https://www.kinopoisk.vip/film/1043758/"
+  },
+  {
+    title: "Волк с Уолл-стрит",
+    year: 2013,
+    rating: 8.2,
+    poster: "https://image.tmdb.org/t/p/w500/34m2tygAYBGqA9MXKhRDtzYd4MR.jpg",
+    link: "https://www.kinopoisk.vip/film/462682/"
+  },
+  {
+    title: "Одержимость",
+    year: 2014,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/7fn624j5lj3xTme2SgiLCeuedmO.jpg",
+    link: "https://www.kinopoisk.vip/film/725190/"
+  },
+  {
+    title: "Семь",
+    year: 1995,
+    rating: 8.6,
+    poster: "https://image.tmdb.org/t/p/w500/6yoghtyTpznpBik8EngEmJskVUO.jpg",
+    link: "https://www.kinopoisk.vip/film/377/"
+  },
+  {
+    title: "Престиж",
+    year: 2006,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/bdN3gXuIZYaJP7ftKK2sU0nPtEA.jpg",
+    link: "https://www.kinopoisk.vip/film/195334/"
+  },
+  {
+    title: "Помни",
+    year: 2000,
+    rating: 8.4,
+    poster: "https://image.tmdb.org/t/p/w500/fKTPH2WvH8nHTXeBYBVhawtRqtR.jpg",
+    link: "https://www.kinopoisk.vip/film/335/"
+  },
+  {
+    title: "Леон",
+    year: 1994,
+    rating: 8.7,
+    poster: "https://image.tmdb.org/t/p/w500/yI6X2cCM5YPJtxMhUd3dPGqDAhw.jpg",
+    link: "https://www.kinopoisk.vip/film/389/"
+  },
+  {
+    title: "Терминатор 2",
+    year: 1991,
+    rating: 8.5,
+    poster: "https://image.tmdb.org/t/p/w500/5M0j0B18abtBI5gi2RhfjjurTqb.jpg",
+    link: "https://www.kinopoisk.vip/film/444/"
+  },
+  {
+    title: "Назад в будущее",
+    year: 1985,
+    rating: 8.6,
+    poster: "https://image.tmdb.org/t/p/w500/fNOH9f1aA7XRTzl1sAOx9iF553Q.jpg",
+    link: "https://www.kinopoisk.vip/film/476/"
+  },
+  {
+    title: "ВАЛЛ-И",
+    year: 2008,
+    rating: 8.4,
+    poster: "https://image.tmdb.org/t/p/w500/hbhFnRzzg6ZDmm8YAmxBnQpQIPh.jpg",
+    link: "https://www.kinopoisk.vip/film/279102/"
+  },
+  {
+  title: "Брат",
+  year: 1997,
+  rating: 8.3,
+  poster: "https://image.tmdb.org/t/p/w500/6csgJ9L0K7wN8Iky3Uczdlz7YT1.jpg",
+  link: "https://www.kinopoisk.vip/film/41519/"
+}
 ];
 
 // API фильмов
